ba8649466fb8f26f22cc57484652c4c621e8d151
Oleksandr Kozlov <Oleksandr.Kozlov@halliburton.com>
2019-02-05 21:13:58 +0200

    merge two arrays using generators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gen-func';
+  title = 'Merge Arrays';
+
+  private mergeResult1: any;
+  private mergeResult2: any;
+
+  private array1: number[] = [1, 7, 11, 17];
+  private array2: number[] = [3, 5, 13];
+  private array3: number[] = [2, 3, 5, 7, 11];
+  private array4: number[] = [2, 4, 6, 8, 10, 12, 14];
+
+  constructor() {}
+
+  *mergeArrays(array1: number[], array2: number[]) {
+    yield array1.concat(array2).sort((n1, n2) => n1 - n2);
+  }
+
+  merge(): void {
+    this.mergeResult1 = this.mergeArrays(this.array1, this.array2).next().value;
+    console.log(this.mergeResult1);
+
+    this.mergeResult2 = this.mergeArrays(this.array3, this.array4).next().value;
+    console.log(this.mergeResult2);
+  }
+
 }
